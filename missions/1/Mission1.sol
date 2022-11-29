d4ab316dbac2a169822c904c71aa31ec2d9bb58a
Deveshb15 <deveshb583@gmail.com>
2022-11-29 13:51:21 +0530

    fix: can't mint same candy multiple times

--- missions/1/Mission1.sol
+++ missions/1/Mission1.sol
@@ -9,12 +9,13 @@ contract Pinata is ERC1155 {
 
     mapping(uint256 => bool) minted;
 
-    constructor() ERC1155("ipfs://QmefoZxEEjZAg2goeXVTQaLDneoJmAgkcCAkv1Qs7BnRQJ/{id}") {}
+    constructor() ERC1155("ipfs://QmQt7PrFkinoSRRDDauTpDXVroA2jnjcA1HfXD24QA77X8/{id}") {}
 
     function hit(uint256 tokenId) public {
         if(tokenId > 2)  { revert invalidCandyIdNumber(); }
-        if (minted[0] == true && minted[1] == true && minted[2] == true){
-            revert("Pinata is already finished");
+        if(minted[tokenId] == true) { revert("Candy already taken");}
+        if (minted[0] == true || minted[1] == true || minted[2] == true){
+            revert("Pinata has no more candies");
         }
         _mint(msg.sender, tokenId, 5, "");
         minted[tokenId] = true;
