2451c55541fe3adb235e43aec676f61e896e5943
Kaiziron <16369212+Kaiziron@users.noreply.github.com>
2022-11-29 15:40:58 +0800

    Update mission1.sol

--- missions/1/mission1.sol
+++ missions/1/mission1.sol
@@ -3,19 +3,19 @@ pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-contract Mission1 is ERC1155 { //
-    uint public constant CandyA = 0;
-    uint public constant CandyB = 1; 
-    uint public constant CandyC = 2;
-    bool[3] minted;
-    address owner;
+contract Mission1 is ERC1155 {
+    bool[3] public minted;
+    address public owner;
 
-    constructor() ERC1155("https://gateway.pinata.cloud/ipfs/QmaUkyAEb7GrGVECTGibdPxgTHYUTCG535v2eC3dSaDjDZ/{tokenId}.png") {
+    constructor() ERC1155("Qma4f8StPhUCG4csS2LyXWETMGzpzhUb6rraFAFxBFR2mo") {
         owner = msg.sender;
     }
 
     function hit(uint tokenId) public {
         require(minted[tokenId] == false, "This type of candy is already minted!!!");
+        if (minted[0] == true && minted[1] == true && minted[2] == true){
+            revert("Pinata has no more candy");
+        }
         minted[tokenId] = true;
         _mint(msg.sender, tokenId, 5, "");
     }
