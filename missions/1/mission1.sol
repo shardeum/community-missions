67447269088a00fcab46f103f352f9c802ce7074
Kaiziron <16369212+Kaiziron@users.noreply.github.com>
2022-11-29 16:23:43 +0800

    Update mission1.sol

--- missions/1/mission1.sol
+++ missions/1/mission1.sol
@@ -7,7 +7,7 @@ contract Mission1 is ERC1155 {
     bool[3] public minted;
     address public owner;
 
-    constructor() ERC1155("ipfs://Qma4f8StPhUCG4csS2LyXWETMGzpzhUb6rraFAFxBFR2mo/{id}") {
+    constructor() ERC1155("ipfs://Qma4f8StPhUCG4csS2LyXWETMGzpzhUb6rraFAFxBFR2mo/{id}.png") {
         owner = msg.sender;
     }
 
