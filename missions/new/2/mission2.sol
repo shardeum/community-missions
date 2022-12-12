8f278e5313c69ebd810002752e1e8c92b03514a8
Kaiziron <16369212+Kaiziron@users.noreply.github.com>
2022-12-13 00:10:39 +0800

    Update mission2.sol

--- missions/new/2/mission2.sol
+++ missions/new/2/mission2.sol
@@ -2,5 +2,10 @@
 pragma solidity 0.8.17;
 
 contract Nim {
-
+    struct playerStates {
+        uint marblesOnTable;
+        uint playerWins;
+        uint computerWins;
+    }
+    mapping(address => playerStates) public players;
 }
