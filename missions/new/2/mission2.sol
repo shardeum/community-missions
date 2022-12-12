10f37521c8a2534e065af8afbd9f49492028fc82
Kaiziron <16369212+Kaiziron@users.noreply.github.com>
2022-12-13 00:49:05 +0800

    Update mission2.sol

--- missions/new/2/mission2.sol
+++ missions/new/2/mission2.sol
@@ -16,41 +16,41 @@ contract Nim {
     }
 
     function turn(uint marblesAmount) public {
-        bool playerTurn;
         require(players[msg.sender].marblesOnTable > 0, "No more marbles on table, start a new game");
 
         // Validate marblesAmount
         // First turn
         if (players[msg.sender].marblesOnTable == 12) {
-            require(marblesAmount <= 3);
+            require(marblesAmount <= 3, "You can only take 0 to 3 marbles on the first turn");
         }
         // After first turn
         else if (players[msg.sender].marblesOnTable < 12) {
-            require(marblesAmount <= 3 && marblesAmount > 0);
+            require(marblesAmount <= 3 && marblesAmount > 0, "You can only take 1 to 3 marbles after the first turn");
         }
         else { revert("Unexpected"); }
         
         // Player's turn
-        playerTurn = true;
         players[msg.sender].marblesOnTable -= marblesAmount;
         
-        // Computer's turn
-        playerTurn = false;
-        uint _marblesOnTable = players[msg.sender].marblesOnTable;
-        if (_marblesOnTable == 4 || _marblesOnTable == 8 || _marblesOnTable == 12) {
-            players[msg.sender].marblesOnTable -= 1;
-        }
-        players[msg.sender].marblesOnTable -= players[msg.sender].marblesOnTable % 4;
-        if (players[msg.sender].marblesOnTable < 4) {
-            players[msg.sender].marblesOnTable -= players[msg.sender].marblesOnTable;
-        }
-
-        // Add win count if someone wins
-        if (players[msg.sender].marblesOnTable == 0 && playerTurn ) {
+        // Add win count if player wins
+        if (players[msg.sender].marblesOnTable == 0) {
             players[msg.sender].playerWins += 1;
         }
-        else if (players[msg.sender].marblesOnTable == 0 && !playerTurn) {
-            players[msg.sender].computerWins += 1;
+        // Computer's turn if player have not win
+        else {
+            uint _marblesOnTable = players[msg.sender].marblesOnTable;
+            if (_marblesOnTable == 4 || _marblesOnTable == 8 || _marblesOnTable == 12) {
+                players[msg.sender].marblesOnTable -= 1;
+            }
+            players[msg.sender].marblesOnTable -= players[msg.sender].marblesOnTable % 4;
+            if (players[msg.sender].marblesOnTable < 4) {
+                players[msg.sender].marblesOnTable -= players[msg.sender].marblesOnTable;
+            }
+
+            // Add win count if computer wins
+            if (players[msg.sender].marblesOnTable == 0) {
+                players[msg.sender].computerWins += 1;
+            }
         }
     }
 }
