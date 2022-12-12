82bffc6f5318a1196b98a17b65035150881b057a
Kaiziron <16369212+Kaiziron@users.noreply.github.com>
2022-12-13 00:25:30 +0800

    Update mission2.sol

--- missions/new/2/mission2.sol
+++ missions/new/2/mission2.sol
@@ -8,4 +8,20 @@ contract Nim {
         uint computerWins;
     }
     mapping(address => playerStates) public players;
+
+    function newGame() public {
+        if (players[msg.sender].marblesOnTable == 0) {
+            players[msg.sender].marblesOnTable = 12;
+        }
+    }
+
+    function turn(uint marblesAmount) public {
+        // First turn
+        if (players[msg.sender].marblesOnTable == 12) {
+            require(marblesAmount <= 3);
+        }
+        else if (players[msg.sender].marblesOnTable < 12) {
+            require(marblesAmount <= 3 && marblesAmount > 0);
+        }
+    }
 }
