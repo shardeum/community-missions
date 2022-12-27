65300bbbfe78342a8203ff9680c1e4d686ca6f86
moganesan <developer.moganesan>
2022-12-27 08:59:31 +0530

    supradex

--- contracts/OrderBookDex.sol
+++ contracts/OrderBookDex.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "hardhat/console.sol";
